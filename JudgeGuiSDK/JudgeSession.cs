0fc9e5c62aab7c3a884f6240cec2aa89d61922f1
dengbw01 <dengbw01@outlook.com>
2022-02-15 22:08:50 +0800

    Initial Commit

---
+++ JudgeGuiSDK/JudgeSession.cs
@@ -0,0 +1,55 @@
+﻿using JudgeGuiSDK.Packages;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace JudgeGuiSDK
+{
+    public class JudgeSession
+    {
+        Socket sock;
+        IPEndPoint remote;
+        static public bool ThrowErrors = false;
+        Exception LastException;
+        public JudgeSession(IPAddress judgeguiserver, int port = 6666)
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            remote = new IPEndPoint(judgeguiserver, port);
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                sock.Connect(remote);
+            }
+            catch(Exception e)
+            {
+                if (ThrowErrors) throw;
+                LastException = e;
+                return false;
+            }
+            return sock.Connected;
+        }
+
+        public bool SendPack(CommonPackage package)
+        {
+            try
+            {
+                sock.Send(package.Pack());
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (ThrowErrors) throw;
+                LastException = e;
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+            sock.Close();
+        }
+    }
+}
---
+++ JudgeGuiSDKTests/JudgeSessionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JudgeGuiSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace JudgeGuiSDK.Tests
+{
+    [TestClass()]
+    public class JudgeSessionTests
+    {
+        private JudgeSession _session;
+        [TestMethod()]
+        public void JudgeSessionTest()
+        {
+            _session = new JudgeSession(IPAddress.Loopback);
+            _session.Connect();
+            _session.SendPack(new Packages.TeamInfoPackage() { TeamID = "TestTeam" });
+            Thread.Sleep(5000);
+            _session.SendPack(new Packages.ResultPackage(Packages.ResultPackageType.MeasurementResults) {  });
+            _session.Close();
+        }
+    }
+}
